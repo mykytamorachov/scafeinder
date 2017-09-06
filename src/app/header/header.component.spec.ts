1b648dec7991fa5c52ecace1eadf02a8fcee4e66
Oksana Hodysh <ksiyshuk@gmail.com>
2017-09-06 16:24:33 +0300

    Header and App tests modified.

--- src/app/header/header.component.spec.ts
+++ src/app/header/header.component.spec.ts
@@ -1,5 +1,5 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { HeaderComponent } from './header.component';
 
 describe('HeaderComponent', () => {
@@ -8,6 +8,7 @@ describe('HeaderComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [ NgbModule ],
       declarations: [ HeaderComponent ]
     })
     .compileComponents();
