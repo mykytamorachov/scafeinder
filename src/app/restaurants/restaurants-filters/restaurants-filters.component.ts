3eb465c6a97800489b33051e0183f369462704e2
Oksana Hodysh <ksiyshuk@gmail.com>
2017-09-13 13:23:49 +0300

    Filter-component modified.

--- src/app/restaurants/restaurants-filters/restaurants-filters.component.ts
+++ src/app/restaurants/restaurants-filters/restaurants-filters.component.ts
@@ -6,10 +6,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./restaurants-filters.component.scss']
 })
 export class RestaurantsFiltersComponent implements OnInit {
-
+  categories: string[] = ['alcohol-free', 'barbecue', 'cafe', 'coffee house', 'fast food', 'lounge bar', 'pizza', 'pub', 'sushi'];
+  cuisines: string[] = ['Asian', 'Chinese', 'Georgian',
+  'Italian', 'Japanese', 'Mediterranean', 'Mexican', 'Ukrainian', 'Vegetarian'];
+  features: string[] = ['hookah', 'karaoke', 'live music', 'roof terrace'];
+  cuisineFilter: string[] = [];
   constructor() { }
 
   ngOnInit() {
   }
 
+  updateCategoryFilter(option, event) {
+    console.log('Category:' + option);
+    console.log(event.target.checked);
+  }
+
+  updateCuisineFilter(option, event) {
+    console.log('Cuisine:' + option);
+    console.log(event.target.checked);
+    if (event.target.checked) {
+      this.cuisineFilter.push(option);
+    } else {
+      const index = this.cuisineFilter.indexOf(option);
+      if (index > -1) {
+        this.cuisineFilter.splice(index, 1);
+      }
+    }
+  }
+
+  updateFeatureFilter(option, event) {
+    console.log('Feature:' + option);
+    console.log(event.target.checked);
+  }
 }
