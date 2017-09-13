2018dfb84630bdb21599952065c1a03972312c91
romua <romafromukraine@gmail.com>
2017-09-13 23:52:04 +0300

    Add banner

---
+++ src/app/components/banner/banner.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { NgbCarouselConfig } from '@ng-bootstrap/ng-bootstrap';
+
+@Component({
+  selector: 'app-banner',
+  templateUrl: './banner.component.html',
+  styleUrls: ['./banner.component.scss'],
+  providers: [NgbCarouselConfig]
+})
+export class BannerComponent implements OnInit {
+
+  constructor(config: NgbCarouselConfig) {
+    config.interval = 4000;
+  }
+
+  ngOnInit() {
+  }
+
+}
