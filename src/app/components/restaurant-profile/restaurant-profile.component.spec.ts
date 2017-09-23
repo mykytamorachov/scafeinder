9f162ae59815caacc7ad40a64756c903340d6372
romua <romafromukraine@gmail.com>
2017-09-23 14:26:44 +0300

    Edit res profile

--- src/app/components/restaurant-profile/restaurant-profile.component.spec.ts
+++ src/app/components/restaurant-profile/restaurant-profile.component.spec.ts
@@ -1,9 +1,10 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RestaurantProfileComponent } from './restaurant-profile.component';
 import { GetCafesService } from '../../services/getcafes/getcafes.service';
 import { HttpModule } from '@angular/http';
 import { RouterTestingModule } from '@angular/router/testing';
+import { NgbTabsetConfig } from '@ng-bootstrap/ng-bootstrap';
 
 describe('RestaurantProfileComponent', () => {
   let component: RestaurantProfileComponent;
@@ -12,8 +13,8 @@ describe('RestaurantProfileComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [ RestaurantProfileComponent ],
-      imports: [ HttpModule, RouterTestingModule ],
-      providers: [ GetCafesService ]
+      imports: [ HttpModule, RouterTestingModule, NgbModule ],
+      providers: [ GetCafesService, NgbTabsetConfig ]
     })
     .compileComponents();
   }));
