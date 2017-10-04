5a7d939cb2b2a54a3e0de00d9bab06e0f5ac540b
MaksymI <maksym.izmailov.lv@gmail.com>
2017-10-04 19:24:25 +0300

    mock GetCafesService

--- src/app/components/restaurants-filters/restaurants-filters.component.spec.ts
+++ src/app/components/restaurants-filters/restaurants-filters.component.spec.ts
@@ -1,19 +1,36 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
+
 import { GetCafesService } from '../../services/getcafes/getcafes.service';
 import { HttpModule } from '@angular/http';
 import { FilterService } from '../../services/filter.service';
+import { cafesJson } from '../../services/getcafes/cafe.data';
 
 import { RestaurantsFiltersComponent } from './restaurants-filters.component';
 
+const getCafesServiceStub = {
+  getAllCafes() {
+    return cafesJson;
+  },
+  getCafeById(id) {
+    return cafesJson.filter(cafe => cafe._id === id);
+  }
+};
+
 describe('RestaurantsFiltersComponent', () => {
-  let component: RestaurantsFiltersComponent;
-  let fixture: ComponentFixture<RestaurantsFiltersComponent>;
+  let component: RestaurantsFiltersComponent,
+      fixture: ComponentFixture<RestaurantsFiltersComponent>,
+      de: DebugElement,
+      el: HTMLElement,
+
+      getCafesService: GetCafesService;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [HttpModule],
       declarations: [RestaurantsFiltersComponent],
-      providers: [GetCafesService, FilterService]
+      providers: [{ provide: GetCafesService, useValue: getCafesServiceStub }, FilterService]
     })
     .compileComponents();
   }));
@@ -21,10 +38,24 @@ describe('RestaurantsFiltersComponent', () => {
   beforeEach(() => {
     fixture = TestBed.createComponent(RestaurantsFiltersComponent);
     component = fixture.componentInstance;
+
+    // Получаем реальный сервис из инжектора компонента
+    getCafesService = fixture.debugElement.injector.get(GetCafesService);
+    // Получаем input-ы c name="categories" по css селектору
+    // de = fixture.debugElement.query(By.css('input[name="categories"]'));
+    de = fixture.debugElement.query(By.css('.custom-control-description')[0]);
+
+    el = de.nativeElement;
     fixture.detectChanges();
   });
 
   it('should be created', () => {
     expect(component).toBeTruthy();
   });
+
+  // it('should return first category = alcohol-free', () => {
+  //   // fixture.detectChanges();
+  //   expect(el.textContent).toContain('alcohol-free');
+  // });
+
 });
