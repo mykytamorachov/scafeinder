f86639d779bf7efdc7ab93da9e58b8b8f5ed398e
Anton <antonzhygalov@gmail.com>
2017-10-10 00:14:59 +0300

    Our App

--- server/constants/database_config.ts
+++ server/constants/database_config.ts
@@ -1,5 +1,5 @@
 export default {
     DATABASE_LOCAL: 'mongodb://127.0.0.1:27017/scafeinder',
     DATABASE_CLOUD: 'mongodb://scafeinder:efacsredni@ds133004.mlab.com:33004/scafeinder',
-    PORT: ''
+    PORT: '5000'
 };
